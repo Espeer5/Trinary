cabf778fdf0685c08e4ddee51e3d65985e3e8b69
Edward Speer <espeer@caltech.edu>
2023-08-03 10:18:35 -0700

    Use Prettier for code formatting and run in workflow (#16)

--- src/CPU/cpu.js
+++ src/CPU/cpu.js
@@ -2,7 +2,7 @@
  * This file instantiates the trinary CPU with all necessary hardware structures
  */
 
-import {Word} from "../representation/word.js"
+import { Word } from "../representation/word.js";
 import { ALU } from "./alu.js";
 import { IOBus } from "../representation/IOBus.js";
 import { AbsractDevice } from "../representation/device.js";
@@ -13,22 +13,22 @@ import { AbsractDevice } from "../representation/device.js";
  * unit, PAU, and DAU. TODO: Add modules and complete docs
  */
 export class CPU {
-    // One-word accumulator
-    accumulator = new Word();
+  // One-word accumulator
+  accumulator = new Word();
 
-    // One-word flags register
-    flags = new Word();
+  // One-word flags register
+  flags = new Word();
 
-    constructor() {
-        let aluIn1 = new IOBus();
-        let aluIn2 = new IOBus();
-        let aluControl = new IOBus();
-        this.alu = new ALU(aluIn1, aluIn2, aluControl);
-    }
+  constructor() {
+    let aluIn1 = new IOBus();
+    let aluIn2 = new IOBus();
+    let aluControl = new IOBus();
+    this.alu = new ALU(aluIn1, aluIn2, aluControl);
+  }
 
-    compute() {
-        this.alu.compute();
-        console.log(this.alu.output.readBus());
-        this.accumulator.writeWord(this.alu.output.readBus());
-    }
+  compute() {
+    this.alu.compute();
+    console.log(this.alu.output.readBus());
+    this.accumulator.writeWord(this.alu.output.readBus());
+  }
 }
