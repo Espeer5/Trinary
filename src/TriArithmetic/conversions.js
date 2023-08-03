b19550b1ccdf8202927db9eab8668f5cf445dd3e
Edward Speer <espeer@caltech.edu>
2023-08-03 09:53:20 -0700

    Perform linting

--- src/TriArithmetic/conversions.js
+++ src/TriArithmetic/conversions.js
@@ -1,7 +1,7 @@
 /**
- * Utility functions for converting back and forth between tri and other 
+ * Utility functions for converting back and forth between tri and other
  * representations
-*/
+ */
 
 import { WORD_SIZE } from "../constants.js";
 import { Word } from "../representation/word.js";
@@ -9,15 +9,15 @@ import { Tri } from "../representation/tri.js";
 
 /**
  * Covert a word to a decimal number. The word must be of size WORD_SIZE.
- * 
- * @param {Word} word 
+ *
+ * @param {Word} word
  * @returns {number} - the decimal value of the word
  */
 export function wordToDec(word) {
-    let dec = 0;
-    let wordVals = word.readWord();
-    for (let i = 0; i < WORD_SIZE; i++) {
-        dec += wordVals[i].state * (3 ** i); 
-    }
-    return dec;
+  let dec = 0;
+  let wordVals = word.readWord();
+  for (let i = 0; i < WORD_SIZE; i++) {
+    dec += wordVals[i].state * 3 ** i;
+  }
+  return dec;
 }
