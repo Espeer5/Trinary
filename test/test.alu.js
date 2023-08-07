ede0d95855a46a3012593db58f5b5137ff5c3aef
Edward Speer <espeer@caltech.edu>
2023-08-07 16:46:47 -0700

    Adding some tests for decimalToBalancedTernary

--- test/test.alu.js
+++ test/test.alu.js
@@ -1,8 +1,15 @@
 import { CPU } from "../src/CPU/cpu.js";
-import { wordToDec } from "../src/TriArithmetic/conversions.js";
+import { Tri } from "../src/representation/tri.js";
+import { wordToDec, decimalToBalancedTernary } from "../src/TriArithmetic/conversions.js";
+import assert from "assert";
+import { WORD_SIZE } from "../src/constants.js";
 
 console.log("running");
-let cpu = new CPU();
+/* let cpu = new CPU();
+
+for(let i = 0; i <= 5; i++) {
+    console.log(decimalToBalancedTernary(i));
+}
 
 cpu.alu.inputs[0].setTri(0, 1);
 cpu.alu.inputs[1].setTri(0, 1);
@@ -18,4 +25,25 @@ cpu.alu.inputs[0].setTri(0, 0);
 cpu.alu.inputs[1].setTri(0, 0);
 cpu.alu.inputs[0].setTri(1, 1);
 cpu.compute();
-console.log(cpu.accumulator.toString());
+console.log(cpu.accumulator.toString()); */
+
+let tri_zero = []
+let tri_minus_one = []
+let tri_one = []
+
+for (let i = 0; i < WORD_SIZE; i++) {
+    tri_zero[i] = new Tri();
+    tri_minus_one[i] = new Tri();
+    tri_one[i] = new Tri();
+}
+
+tri_minus_one[0].setState(-1)
+tri_one[0].setState(1)
+
+describe("decimalToBalancedTernary", function() {
+    it("should convert decimal numbers to balanced ternary representation", function() {
+        assert.equal(decimalToBalancedTernary(0), tri_zero);
+        assert.equal(decimalToBalancedTernary(1), tri_one);
+        assert.equal(decimalToBalancedTernary(-1), tri_minus_one);
+    });
+});
