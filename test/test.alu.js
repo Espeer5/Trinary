5fd885f8abbfdb07596b0a7505a1b1018a8351a6
Edward Speer <espeer@caltech.edu>
2023-08-11 15:06:05 -0700

    Add code coverage reporting (#21)
    * Add codecov reporting

--- test/test.alu.js
+++ test/test.alu.js
@@ -5,20 +5,3 @@ import assert from "assert";
 import { WORD_SIZE } from "../src/constants.js";
 
 console.log("running");
-let cpu = new CPU();
-
-cpu.alu.inputs[0].setTri(0, 1);
-cpu.alu.inputs[1].setTri(0, 1);
-cpu.alu.signal_lines.setTri(0, 0);
-cpu.alu.signal_lines.setTri(1, 0);
-cpu.alu.signal_lines.setTri(2, 0);
-cpu.alu.signal_lines.setTri(3, 1);
-cpu.alu.signal_lines.setTri(4, -1);
-cpu.alu.signal_lines.setTri(5, -1);
-cpu.compute();
-console.log(cpu.accumulator.toString());
-cpu.alu.inputs[0].setTri(0, 0);
-cpu.alu.inputs[1].setTri(0, 0);
-cpu.alu.inputs[0].setTri(1, 1);
-cpu.compute();
-console.log(cpu.accumulator.toString());
